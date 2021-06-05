a41650782c9e6932cdbe93bb705308b8689ff8bf
Gerasimos Papachronopoulos <58421904+geras-papax@users.noreply.github.com>
2021-06-05 18:25:38 +0300

    Update getVote.php

--- project_code/VoteGR-Backend/getVote.php
+++ project_code/VoteGR-Backend/getVote.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_SERVER', 'localhost:3306');
     define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', 'pao134ever');
+    define('DB_PASSWORD', '****');
     define('DB_DATABASE', 'votegr');
     $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
