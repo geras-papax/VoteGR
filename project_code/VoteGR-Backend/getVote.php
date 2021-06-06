a5f21f4b0dfecad0566c5212240b353932b48596
Agisilaos Kounelis <kounelisagis@gmail.com>
2021-06-06 03:02:08 +0300

    getVote charset fix

--- project_code/VoteGR-Backend/getVote.php
+++ project_code/VoteGR-Backend/getVote.php
@@ -1,22 +1,33 @@
 <?php
-    define('DB_SERVER', 'localhost:3306');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '****');
-    define('DB_DATABASE', 'votegr');
-    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+	define('DB_SERVER', '127.0.0.1:3306');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_DATABASE', 'votegr');
 
-    $todate = date("Y-m-d");
-    $sql = "SELECT `vote`.`idvote`,
-    `vote`.`subject`,
-    `vote`.`starting_date`,
-    `vote`.`ending_date`
-    FROM `votegr`.`vote` WHERE `vote`.`starting_date`<= '$todate' AND `vote`.`ending_date`>= '$todate' ;";
+	try {
+		if ($db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE)) {
+			$db->set_charset("utf8");
 
-    $loc = array();
+			$todate = date("Y-m-d");
+			$sql = "SELECT `vote`.`idvote`,
+			`vote`.`subject`,
+			`vote`.`starting_date`,
+			`vote`.`ending_date`
+			FROM `votegr`.`vote` WHERE `vote`.`starting_date`<= '$todate' AND `vote`.`ending_date`>= '$todate' ;";
 
-    $stmt = mysqli_query($db, $sql);
-    $data = mysqli_fetch_assoc($stmt);
-    
-    echo json_encode($data,JSON_UNESCAPED_UNICODE);
+
+			$stmt = mysqli_query($db, $sql);
+			$data = mysqli_fetch_assoc($stmt);
+
+
+			echo json_encode($data, JSON_UNESCAPED_UNICODE);
+		}
+		else {
+			throw new Exception('Unable to connect');
+		}
+	}
+	catch(Exception $e) {
+		echo $e->getMessage();
+	}
+
 ?>
-
