3f42b6a7416cd0bd9e64f9b53af21501a42c5cac
Gerasimos Papachronopoulos <makis.papaxronopoulos@gmail.com>
2021-06-11 11:38:18 +0300

    voting functionality fixed
    Closes #35

--- project_code/VoteGR/app/src/main/java/com/example/votegr/ui/statement/StatementFragment.java
+++ project_code/VoteGR/app/src/main/java/com/example/votegr/ui/statement/StatementFragment.java
@@ -1,11 +1,16 @@
 package com.example.votegr.ui.statement;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -15,26 +20,96 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.votegr.AdminActivity;
+import com.example.votegr.PartyActivity;
 import com.example.votegr.R;
 import com.example.votegr.ui.vote.VoteViewModel;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class StatementFragment extends Fragment {
 
     private StatementViewModel statementViewModel;
+    private Context mContext;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         statementViewModel =
                 new ViewModelProvider(this).get(StatementViewModel.class);
         View root = inflater.inflate(R.layout.party_fragment_statement, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_vote_question);
-        voteViewModel.getVoteQuestion().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
+        this.mContext = getContext();
+
+        TextInputEditText textView = root.findViewById(R.id.party_view);
+        Button button = root.findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                RequestQueue queue =  Volley.newRequestQueue(getActivity().getApplicationContext());
+
+                String url = "http://192.168.2.4/VoteGR/postView.php";
+                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response) {
+                                // response
+                                Log.d("Response", response);
+                                Intent intent = new Intent(getActivity(), PartyActivity.class);
+                                startActivity(intent);
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("Error.Response", error.toString());
+                                Intent intent = new Intent(getActivity(), PartyActivity.class);
+                                startActivity(intent);
+                            }
+                        }
+                ) {
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        SharedPreferences sharedPref = mContext.getSharedPreferences("user_data", Context.MODE_PRIVATE);
+                        String user_detail = sharedPref.getString("user_detail", "Not Found");
+
+                        Map<String, String>  params = new HashMap<String, String>();
+                        params.put("username", user_detail);
+                        params.put("party_opinion", textView.getText().toString());
+
+                        return params;
+                    }
+                };
+                queue.add(postRequest);
             }
-        });*/
+        });
+
+        //textView.getText().toString()
+
+        /*
+            // save user_detail & password
+            SharedPreferences sharedPref = getSharedPreferences("user_data", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("user_detail", user_detail.getText().toString());
+            editor.putString("password", password.getText().toString());
+            editor.apply();
+
+         */
+
+
         return root;
     }
 
--- project_code/VoteGR/app/src/main/java/com/example/votegr/ui/vote/VoteFragment.java
+++ project_code/VoteGR/app/src/main/java/com/example/votegr/ui/vote/VoteFragment.java
@@ -1,10 +1,14 @@
 package com.example.votegr.ui.vote;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,17 +21,66 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.votegr.AdminActivity;
+import com.example.votegr.CitizenActivity;
 import com.example.votegr.R;
+import com.example.votegr.ui.statement.StatementViewModel;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class VoteFragment extends Fragment {
 
     private VoteViewModel voteViewModel;
+    private Context mContext;
 
-    void getVoting(TextView textView) {
+    private void sendVote(String choice) {
+
+        RequestQueue queue =  Volley.newRequestQueue(getActivity().getApplicationContext());
+
+        String url = "http://192.168.2.4/VoteGR/voting.php";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                        Intent intent = new Intent(getActivity(), CitizenActivity.class);
+                        startActivity(intent);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", error.toString());
+                        Intent intent = new Intent(getActivity(), CitizenActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                SharedPreferences sharedPref = mContext.getSharedPreferences("user_data", Context.MODE_PRIVATE);
+                String user_detail = sharedPref.getString("user_detail", "Not Found");
+
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("user_detail", user_detail);
+                params.put("choice", choice);
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
+
+    }
+
+    private void getVoting(TextView textView) {
         RequestQueue queue =  Volley.newRequestQueue(getActivity().getApplicationContext());
         String url ="http://192.168.2.4/VoteGR/getVote.php";
 
@@ -60,13 +113,33 @@ public class VoteFragment extends Fragment {
     }
 
 
-
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         voteViewModel =
                 new ViewModelProvider(this).get(VoteViewModel.class);
         View root = inflater.inflate(R.layout.citizen_fragment_vote, container, false);
         final TextView textView = root.findViewById(R.id.text_vote_question);
+        this.mContext = getContext();
+
+        Button buttonYes = root.findViewById(R.id.button_yes);
+        Button buttonNo = root.findViewById(R.id.button_no);
+        Button buttonWhite = root.findViewById(R.id.button_white);
+
+        buttonYes.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendVote("yes");
+            }
+        });
+        buttonNo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendVote("no");
+            }
+        });
+        buttonWhite.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendVote("white");
+            }
+        });
 
         getVoting(textView);
 
